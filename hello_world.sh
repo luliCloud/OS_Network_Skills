abc56338e01a24921defb7f3c3ed52747a2675d5
Lu Li <greenpearll0804@gmail.com>
2024-04-30 22:07:51 -0700

    write hello world sh

---
+++ hello_world.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script outputs "Hello World" followed by the current date and time
+
+# print "Hello, World"
+echo "Hello, World"
+
+# print the current date and time
+echo "Current date and time: $(date)"
+
+# how to run this file
+# let a: owner; g: group member of this file belongs to; o: other user in this system
+# to have the permission to execute this file
+# chmod +x hello_world.sh 
+# ./hello_world.sh  
+# noting: no need to cd.., similar to ./a.out in C++
