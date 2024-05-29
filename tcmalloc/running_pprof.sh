2cf009efa745887c7067a92e57a4cc5ab7431a4b
Lu Li <greenpearll0804@gmail.com>
2024-05-29 00:25:58 -0700

    running pprof using env var

---
+++ tcmalloc/running_pprof.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# 设置内存剖析文件的路径
+PROFILE_PATH="memory_profile"
+
+# 导出 HEAPPROFILE 环境变量
+export HEAPPROFILE="${PROFILE_PATH}"
+
+# compile and running one of your program
+g++ -o supervise.out supervise_mallocextension.cpp -ltcmalloc
+./supervise.out
+
+# running this program
+# chmod +x running_pprof.sh
+# ./running_pprof.sh
